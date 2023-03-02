01e977466d0bc50e00f7811d58b59c1d1f79948a
DmitryGuzov <dev@anuitex.net>
2023-03-02 10:47:08 +0200

    changed dialogsApi

--- chat-frontend/src/utils/api/dialogs.ts
+++ chat-frontend/src/utils/api/dialogs.ts
@@ -2,7 +2,7 @@ import { axios } from "../../core";
 import { Dialog } from "../../redux/dialogs/types";
 
 const dialogsApi = {
-    getAll: () => axios.get<Array<Dialog>>("/dialogs")
+    getAll: () => axios.get<Array<Dialog>>("/dialogs/all")
         .then(response => response.data)
 };
 
